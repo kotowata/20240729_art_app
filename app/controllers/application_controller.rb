9372cfd61ddbc2a3c4c1bc26746592f85818a2e9
Kotono Watanabe <kotakoto529@gmail.com>
2024-08-18 00:40:22 +0900

    Add: 未ログインユーザーはログインパスへリダイレクトする

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,9 @@
 class ApplicationController < ActionController::Base
-  # Only allow modern browsers supporting webp images, web push, badges, import maps, CSS nesting, and CSS :has.
-  allow_browser versions: :modern
+  before_action :require_login
+
+  private
+
+  def not_authenticated
+    redirect_to login_path
+  end
 end
