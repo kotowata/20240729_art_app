25e4f24bee3f09946478cb69e4d8ec15f944c3d1
Kotono Watanabe <kotakoto529@gmail.com>
2024-08-18 00:29:19 +0900

    Add: ユーザー登録用ルーティング設定

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   root "static_pages#top"
+  resources :users, only: %i[new create]
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
   # Can be used by load balancers and uptime monitors to verify that the app is live.
