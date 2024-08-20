a17cef3bc0ae4caa2e6ccf433dba3c62460f4f30
Kotono Watanabe <kotakoto529@gmail.com>
2024-08-20 14:47:00 +0900

    Fix: ログアウトリンクのヘルパーメソッド修正し、turbo_method追記

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,7 @@ Rails.application.routes.draw do
   resources :users, only: %i[new create]
   get "login", to: "user_sessions#new"
   post "login", to: "user_sessions#create"
-  delete "login", to: "user_sessions#destroy"
+  delete "logout", to: "user_sessions#destroy"
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
   # Can be used by load balancers and uptime monitors to verify that the app is live.
