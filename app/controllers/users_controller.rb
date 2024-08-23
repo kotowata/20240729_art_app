af04b47075f9bd44f024c3fc67650f53c8a9369f
Kotono Watanabe <kotakoto529@gmail.com>
2024-08-23 13:13:45 +0900

    Fix: ユーザー登録完了時のリダイレクト先をログインページに変更

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,7 +8,7 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
-      redirect_to root_path, success: t("users.create.success")
+      redirect_to login_path, success: t("users.create.success")
     else
       flash.now[:failure] = t("users.create.failure")
       render :new, status: :unprocessable_entity
