8b6949a964528e8de9c4f62eae5acdb32052a58b
Kotono Watanabe <kotakoto529@gmail.com>
2024-08-24 15:25:47 +0900

    Update: ポスト機能

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,7 +1,7 @@
 class Post < ApplicationRecord
   validates :title, presence: true, length: { maximum: 255 }
   validates :content, length: { maximum: 65_535 }
-  validates :event_url, length: { maximum: 255 }, format: { with: URI::regexp(%w[http https]), message: "無効なURLです" }
+  validates :event_url, length: { maximum: 255 }, format: { with: URI::regexp(%w[http https]), message: "無効なURLです", allow_blank: true }
 
   belongs_to :user
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,18 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+2.times do
+  User.create!(last_name: Faker::Name.last_name,
+              first_name: Faker::Name.first_name,
+              nick_name: Faker::Internet.username,
+              email: Faker::Internet.unique.email,
+              password: "password",
+              password_confirmation: "password")
+end
+
+user_ids = User.ids
+
+5.times do |index|
+  user = User.find(user_ids.sample)
+  user.posts.create!(title: "タイトル#{index}", content: "本文#{index}", event_url: "")
+end
