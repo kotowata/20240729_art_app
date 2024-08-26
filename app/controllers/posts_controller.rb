8ec0710ed8175b95759340b5aff4b1a373653cb8
Kotono Watanabe <kotakoto529@gmail.com>
2024-08-26 13:03:08 +0900

    Add: 掲示板作成処理を実装

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -8,4 +8,20 @@ class PostsController < ApplicationController
   def new
     @post = Post.new
   end
+
+  def create
+    @post = current_user.posts.build(post_params)
+    if @post.save
+      redirect_to posts_path, success: t("defaults.flash_message.created", item: Post.model_name.human)
+    else
+      flash.now[:danger] = t("defaults.flash_message.not_created", item: Post.model_name.human)
+      render :new, status: unprocessable_entity
+    end
+  end
+
+  private
+
+  def post_params
+    params.require(:post).permit(:title, :content, :event_url)
+  end
 end
