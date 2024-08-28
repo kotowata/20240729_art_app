efa3b12b841ddf21d6c6121bffdce6e82bdf0ecf
Kotono Watanabe <kotakoto529@gmail.com>
2024-08-28 11:45:33 +0900

    Fix: Lint

--- app/uploaders/post_image_uploader.rb
+++ app/uploaders/post_image_uploader.rb
@@ -33,8 +33,7 @@ class PostImageUploader < CarrierWave::Uploader::Base
   # Add an allowlist of extensions which are allowed to be uploaded.
   # For images you might use something like this:
   def extension_allowlist
-    %w(jpg jpeg gif png)
-  end
+    %w[jpg jpeg gif png]
 
   # Override the filename of the uploaded files:
   # Avoid using model.id or version_name here, see uploader/store.rb for details.
