4d6aa6e30464c6cbf3e1e56de95efacb645bc2c1
Kotono Watanabe <kotakoto529@gmail.com>
2024-08-28 17:31:31 +0900

    Add: postテーブルに期間カラム追加実行

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.2].define(version: 2024_08_22_054943) do
+ActiveRecord::Schema[7.2].define(version: 2024_08_28_082652) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -22,6 +22,8 @@ ActiveRecord::Schema[7.2].define(version: 2024_08_22_054943) do
     t.bigint "user_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.date "start_date"
+    t.date "end_date"
     t.index ["user_id"], name: "index_posts_on_user_id"
   end
 
