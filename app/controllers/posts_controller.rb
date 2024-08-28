5cd424f949ce6e053586754b476be97e54046793
Kotono Watanabe <kotakoto529@gmail.com>
2024-08-28 11:29:13 +0900

    Add: 画像投稿機能実装

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -22,6 +22,6 @@ class PostsController < ApplicationController
   private
 
   def post_params
-    params.require(:post).permit(:title, :content, :event_url)
+    params.require(:post).permit(:title, :content, :event_url, :post_image, :post_image_cache)
   end
 end
