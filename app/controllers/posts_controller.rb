79cbcbbf855f51e86139f369b33b6b3a2cae96b8
Kotono Watanabe <kotakoto529@gmail.com>
2024-08-28 13:52:17 +0900

    Add: ポスト詳細機能実装

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -19,6 +19,10 @@ class PostsController < ApplicationController
     end
   end
 
+  def show
+    @post = Post.find(params[:id])
+  end
+
   private
 
   def post_params
--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   root "static_pages#top"
   resources :users, only: %i[new create]
-  resources :posts, only: %i[new create index]
+  resources :posts, only: %i[new create index show]
   get "login", to: "user_sessions#new"
   post "login", to: "user_sessions#create"
   delete "logout", to: "user_sessions#destroy"
