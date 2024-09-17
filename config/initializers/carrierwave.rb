5e5b6ab32e31bed7d13a7fab4334d05dc4730216
Kotono Watanabe <kotakoto529@gmail.com>
2024-09-17 20:00:17 +0900

    Test: 設定修正

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -5,7 +5,7 @@ require "carrierwave/storage/fog"
 CarrierWave.configure do |config|
   if Rails.env.production?
     config.storage :fog
-    config.fog_provider = "fog/aws"
+    # config.fog_provider = "fog/aws"
     config.fog_directory  = ENV["S3_BUCKET_NAME"]
     config.fog_public = false
     config.fog_credentials = {
