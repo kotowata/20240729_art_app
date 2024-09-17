fdde27f702b5b08ac433ec4de6da3acf9156d7e5
Kotono Watanabe <kotakoto529@gmail.com>
2024-09-17 19:33:44 +0900

    Test: バケット作成し直し

--- app/uploaders/post_image_uploader.rb
+++ app/uploaders/post_image_uploader.rb
@@ -4,8 +4,12 @@ class PostImageUploader < CarrierWave::Uploader::Base
   # include CarrierWave::MiniMagick
 
   # Choose what kind of storage to use for this uploader:
-
-  storage :fog
+  if Rails.env.production?
+    storage :fog
+  else
+    storage :file
+  end
+  # storage :fog
 
   # Override the directory where uploaded files will be stored.
   # This is a sensible default for uploaders that are meant to be mounted:
--- app/uploaders/user_icon_uploader.rb
+++ app/uploaders/user_icon_uploader.rb
@@ -5,7 +5,11 @@ class UserIconUploader < CarrierWave::Uploader::Base
 
   # Choose what kind of storage to use for this uploader:
 
-  storage :fog
+  if Rails.env.production?
+    storage :fog
+  else
+    storage :file
+  end
 
   # Override the directory where uploaded files will be stored.
   # This is a sensible default for uploaders that are meant to be mounted:
