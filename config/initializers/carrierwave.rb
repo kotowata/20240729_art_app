e32f2f186a9e47ed900a83d5220b96552963e8c4
Kotono Watanabe <kotakoto529@gmail.com>
2024-09-17 14:24:07 +0900

    Fix: 設定修正

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -1,6 +1,7 @@
 require 'carrierwave/storage/abstract'
 require 'carrierwave/storage/file'
 require 'carrierwave/storage/fog'
+require 'fog/aws'
 
 CarrierWave.configure do |config|
   if Rails.env.production?
