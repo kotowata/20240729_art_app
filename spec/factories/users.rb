377c1245257763cc556b8822995ea11075259dac
Kotono Watanabe <kotakoto529@gmail.com>
2024-09-20 17:35:02 +0900

    Fix: userモデルテストから姓名を削除

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,7 +1,5 @@
 FactoryBot.define do
   factory :user do
-    first_name { Faker::Name.first_name }
-    last_name { Faker::Name.last_name }
     nick_name { Faker::Internet.username }
     email { Faker::Internet.unique.email }
     password { '12345678' }
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -2,7 +2,7 @@ require 'rails_helper'
 
 RSpec.describe User, type: :model do
   describe "バリデーションチェック" do
-    it "姓、名、ニックネーム、メールがあり、パスワードは3文字以上であれば有効であること" do
+    it "ニックネーム、メールがあり、パスワードは3文字以上であれば有効であること" do
       user = build(:user)
       expect(user).to be_valid
     end
@@ -15,27 +15,19 @@ RSpec.describe User, type: :model do
       expect(user2.errors[:email]).to include("はすでに存在します")
     end
 
-    it "姓、名、ニックネーム、メールアドレスは必須項目であること" do
+    it "ニックネーム、メールアドレスは必須項目であること" do
       user = build(:user)
       user.email = nil
-      user.first_name = nil
-      user.last_name = nil
       user.nick_name = nil
       user.valid?
       expect(user.errors[:email]).to include("を入力してください")
-      expect(user.errors[:first_name]).to include("を入力してください")
-      expect(user.errors[:last_name]).to include("を入力してください")
       expect(user.errors[:nick_name]).to include("を入力してください")
     end
 
-    it "姓、名、ニックネームは255文字以下であること" do
+    it "ニックネームは255文字以下であること" do
       user = build(:user)
-      user.first_name = "a" * 256
-      user.last_name = "b" * 256
       user.nick_name = "c" * 256
       user.valid?
-      expect(user.errors[:first_name]).to include('は255文字以内で入力してください')
-      expect(user.errors[:last_name]).to include('は255文字以内で入力してください')
       expect(user.errors[:nick_name]).to include('は255文字以内で入力してください')
     end
   end
