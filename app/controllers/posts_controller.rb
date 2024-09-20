890b62029e50092776634e24d71333630f1dfac6
Kotono Watanabe <kotakoto529@gmail.com>
2024-09-20 16:37:37 +0900

    Update: 投稿詳細も未ログインで閲覧不可能に再変更

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,5 +1,5 @@
 class PostsController < ApplicationController
-  skip_before_action :require_login, only: %i[index show]
+  skip_before_action :require_login, only: %i[index]
 
   def index
     @q = Post.ransack(params[:q])
