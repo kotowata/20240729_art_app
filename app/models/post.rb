59d1a0139dc9735c324023fd9206549f8caf08d4
Kotono Watanabe <kotakoto529@gmail.com>
2024-09-20 10:07:15 +0900

    Update: ポストタイトル、コメント文字数制限変更

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,6 +1,6 @@
 class Post < ApplicationRecord
-  validates :title, presence: true, length: { maximum: 255 }
-  validates :content, length: { maximum: 65_535 }
+  validates :title, presence: true, length: { maximum: 30 }
+  validates :content, length: { maximum: 140 }
   validates :event_url, length: { maximum: 255 }, format: { with: URI.regexp(%w[http https]), message: "無効なURLです", allow_blank: true }
 
   belongs_to :user
