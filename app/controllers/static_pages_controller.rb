f4fefdbe205689adc17a995f57d4310e2a64bf8d
Kotono Watanabe <kotakoto529@gmail.com>
2024-09-21 20:01:11 +0900

    Update: gemなしで利用規約・プライバシーポリシーページ作成し直し

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,5 +1,10 @@
 class StaticPagesController < ApplicationController
-  skip_before_action :require_login, only: %i[top]
+  skip_before_action :require_login, only: %i[top privacy_policy terms]
 
   def top; end
+
+  def privacy_policy; end
+
+  def terms; end
+
 end
