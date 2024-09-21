b9498b8d61d0a47c39fee7ae4caa85ff139620b1
Kotono Watanabe <kotakoto529@gmail.com>
2024-09-21 13:38:13 +0900

    Update: postモデルスペックに日付と所在地に関するテストを追加

--- spec/factories/posts.rb
+++ spec/factories/posts.rb
@@ -4,5 +4,8 @@ FactoryBot.define do
     sequence(:content) { |n| "本文#{n}" }
     event_url { Faker::Internet.url }
     association :user
+    start_date { Date.today }
+    end_date { Date.today + 7.days }
+    prefecture_id { Prefecture.all.sample.id }
   end
 end
--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -16,33 +16,33 @@ RSpec.describe Post, type: :model do
     end
   end
 
-  context "タイトルが255文字以下の場合" do
+  context "タイトルが30文字以下の場合" do
     it "有効であること" do
-      post = build(:post, title: "a" * 255)
+      post = build(:post, title: "a" * 30)
       expect(post).to be_valid
     end
   end
 
-  context "タイトルが255文字以上の場合" do
+  context "タイトルが30文字以上の場合" do
     it "無効であること" do
-      post = build(:post, title: "a" * 256)
+      post = build(:post, title: "a" * 31)
       expect(post).to be_invalid
-      expect(post.errors[:title]).to include('は255文字以内で入力してください')
+      expect(post.errors[:title]).to include('は30文字以内で入力してください')
     end
   end
 
-  context "本文が65535文字以内の場合" do
+  context "本文が140文字以内の場合" do
     it "有効であること" do
-      post = build(:post, content: "a" * 65535)
+      post = build(:post, content: "a" * 140)
       expect(post).to be_valid
     end
   end
 
-  context "本文が65536文字以上の場合" do
+  context "本文が140文字以上の場合" do
     it "無効であること" do
-      post = build(:post, content: "a" * 65536)
+      post = build(:post, content: "a" * 141)
       expect(post).to be_invalid
-      expect(post.errors[:content]).to include("は65535文字以内で入力してください")
+      expect(post.errors[:content]).to include("は140文字以内で入力してください")
     end
   end
 
@@ -70,10 +70,45 @@ RSpec.describe Post, type: :model do
   end
 
   context "URLが無効な形式の場合" do
-    it "無効であること" do
+    fit "無効であること" do
       post = build(:post, event_url: "invalid_url")
       expect(post).to be_invalid
       expect(post.errors[:event_url]).to include("無効なURLです")
+    end
+  end
+
+  context "開始日と終了日が入力されている場合" do
+    it "有効であること" do
+      post = build(:post, start_date: "2024-09-01", end_date: "2024-09-30")
+      expect(post).to be_valid
+    end
+  end
+
+  context "開始日が空欄の場合" do
+    it "有効であること" do
+      post = build(:post, start_date: "")
+      expect(post).to be_valid
+    end
+  end
+
+  context "終了日が空欄の場合" do
+    it "有効であること" do
+      post = build(:post, end_date: "")
+      expect(post).to be_valid
+    end
+  end
+
+  context "所在地が空欄の場合" do
+    it "有効であること" do
+      post = build(:post, prefecture_id: "")
+      expect(post).to be_valid
+    end
+  end
+
+  context "所在地が選択済みの場合" do
+    it "有効であること" do
+      post = build(:post, prefecture_id: "1")
+      expect(post).to be_valid
     end
   end
 end
