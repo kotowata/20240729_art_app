b2316d9ceea95bea5f6349f2a0935e0a14e13853
Kotono Watanabe <kotakoto529@gmail.com>
2024-09-23 21:19:09 +0900

    Add: プロフィール機能実装

---
+++ app/controllers/profiles_controller.rb
@@ -0,0 +1,26 @@
+class ProfilesController < ApplicationController
+  before_action :set_user, only: %i[edit update]
+
+  def edit; end
+
+  def update
+    if @user.update(user_params)
+      redirect_to profile_path, success: t('defaults.flash_message.updated', item: User.model_name.human)
+    else
+      flash.now['danger'] = t('defaults.flash_message.not_updated', item: User.model_name.human)
+      render :edit, status: :unprocessable_entity
+    end
+  end
+
+  def show; end
+
+  private
+
+  def set_user
+    @user = User.find(current_user.id)
+  end
+
+  def user_params
+    params.require(:user).permit(:nick_name, :email, :user_icon, :user_icon_cache)
+  end
+end
