3019c395cbc03250a62c8558bafa10e8b6f54771
Kotono Watanabe <kotakoto529@gmail.com>
2024-09-24 14:47:45 +0900

    Fix: Lint

--- app/controllers/mypages_controller.rb
+++ app/controllers/mypages_controller.rb
@@ -5,9 +5,9 @@ class MypagesController < ApplicationController
 
   def update
     if @user.update(user_params)
-      redirect_to mypage_path, success: t('defaults.flash_message.updated', item: User.model_name.human)
+      redirect_to mypage_path, success: t("defaults.flash_message.updated", item: User.model_name.human)
     else
-      flash.now['danger'] = t('defaults.flash_message.not_updated', item: User.model_name.human)
+      flash.now["danger"] = t("defaults.flash_message.not_updated", item: User.model_name.human)
       render :edit, status: :unprocessable_entity
     end
   end
