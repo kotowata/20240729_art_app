e506d999c2ac80ddf68ee63aad71673db8436d41
Kotono Watanabe <kotakoto529@gmail.com>
2024-09-30 16:17:39 +0900

    Add: routing設定

--- config/routes.rb
+++ config/routes.rb
@@ -23,4 +23,7 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "posts#index"
+  post "oauth/callback" => "oauths#callback"
+  get "oauth/callback" => "oauths#callback"
+  get "oauth/:provider" => "oauths#oauth", as:  :auth_at_provider
 end
