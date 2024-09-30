f0010c7812f185bf92d8a1fa28147cde8e69e4fe
Kotono Watanabe <kotakoto529@gmail.com>
2024-09-30 15:56:43 +0900

    Add: Google認証機能_migration

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.2].define(version: 2024_09_19_084226) do
+ActiveRecord::Schema[7.2].define(version: 2024_09_30_065133) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "authentications", force: :cascade do |t|
+    t.integer "user_id", null: false
+    t.string "provider", null: false
+    t.string "uid", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["provider", "uid"], name: "index_authentications_on_provider_and_uid"
+  end
 
   create_table "posts", force: :cascade do |t|
     t.string "title", null: false
