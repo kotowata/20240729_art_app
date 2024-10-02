f47077b7a837f5ca9671a2672149dd779693f9ce
Kotono Watanabe <kotakoto529@gmail.com>
2024-10-02 19:01:52 +0900

    Fix: Lint

--- db/migrate/20240930065133_sorcery_external.rb
+++ db/migrate/20240930065133_sorcery_external.rb
@@ -7,6 +7,6 @@ class SorceryExternal < ActiveRecord::Migration[7.2]
       t.timestamps              null: false
     end
 
-    add_index :authentications, [:provider, :uid]
+    add_index :authentications, [ :provider, :uid ]
   end
 end
