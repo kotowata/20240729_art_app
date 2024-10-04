8872b0f718cd39128b5201420ff88149826a8cad
Kotono Watanabe <kotakoto529@gmail.com>
2024-10-04 14:52:08 +0900

    Update: マイグレーション設定

--- db/migrate/20241004053044_create_likes.rb
+++ db/migrate/20241004053044_create_likes.rb
@@ -1,7 +1,10 @@
 class CreateLikes < ActiveRecord::Migration[7.2]
   def change
     create_table :likes do |t|
+      t.references :user, foreign_key: true
+      t.references :post, foreign_key: true
       t.timestamps
     end
+    add_index :likes, [:user_id, :post_id], unique: true
   end
 end
