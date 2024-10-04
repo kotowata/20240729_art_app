c0f75c870eaaf8ccdf42f7a563bd3b6a189118d7
Kotono Watanabe <kotakoto529@gmail.com>
2024-10-04 15:18:05 +0900

    Add: アソシエーション設定

--- app/models/user.rb
+++ app/models/user.rb
@@ -10,6 +10,8 @@ class User < ApplicationRecord
   has_many :posts, dependent: :destroy
   has_many :authentications, dependent: :destroy
   accepts_nested_attributes_for :authentications
+  has_many :likes, dependent: :destroy
+  has_many :like_posts, through: :likes, source: :post
 
   mount_uploader :user_icon, UserIconUploader
 
