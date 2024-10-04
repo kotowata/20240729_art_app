061edad993bfc000cfa6d6c101ee4fbd5f96df4c
Kotono Watanabe <kotakoto529@gmail.com>
2024-10-04 19:48:07 +0900

    Fix: Lint

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -2,12 +2,12 @@ class LikesController < ApplicationController
   def create
     post = Post.find(params[:post_id])
     current_user.like(post)
-    redirect_to posts_path, success: t('.success')
+    redirect_to posts_path, success: t(".success")
   end
 
   def destroy
     post = current_user.likes.find(params[:id]).post
     current_user.unlike(post)
-    redirect_to posts_path, success: t('.success'), status: :see_other
+    redirect_to posts_path, success: t(".success"), status: :see_other
   end
 end
