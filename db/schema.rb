b3e9fe45f810cdf282fc389cb52f975572d5dc69
Kotono Watanabe <kotakoto529@gmail.com>
2024-10-09 17:39:03 +0900

    Add: migration

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.2].define(version: 2024_10_04_053044) do
+ActiveRecord::Schema[7.2].define(version: 2024_10_09_082843) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -55,7 +55,12 @@ ActiveRecord::Schema[7.2].define(version: 2024_10_04_053044) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "user_icon"
+    t.string "reset_password_token"
+    t.datetime "reset_password_token_expires_at"
+    t.datetime "reset_password_email_sent_at"
+    t.integer "access_count_to_reset_password_page", default: 0
     t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
   add_foreign_key "likes", "posts"
