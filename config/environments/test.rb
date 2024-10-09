d391dc493fa025ec72d9567f4ff2a3299cdd0c40
Kotono Watanabe <kotakoto529@gmail.com>
2024-10-09 17:30:45 +0900

    Add: letter_opener設定

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -43,6 +43,8 @@ Rails.application.configure do
   # ActionMailer::Base.deliveries array.
   config.action_mailer.delivery_method = :test
 
+  config.action_mailer.default_url_options = { host: "localhost", port: 3000 }
+
   # Unlike controllers, the mailer instance doesn't have any context about the
   # incoming request so you'll need to provide the :host parameter yourself.
   config.action_mailer.default_url_options = { host: "www.example.com" }
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  mount LetterOpenerWeb::Engine, at: "/letter_opener" if Rails.env.development?
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   root "static_pages#top"
   get "terms", to: "static_pages#terms"
