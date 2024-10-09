64100992213d4fb1846bf3733805a4a952f7f482
Kotono Watanabe <kotakoto529@gmail.com>
2024-10-09 20:11:13 +0900

    Add: ルーティング追加

--- config/routes.rb
+++ config/routes.rb
@@ -18,6 +18,7 @@ Rails.application.routes.draw do
   get "login", to: "user_sessions#new"
   post "login", to: "user_sessions#create"
   delete "logout", to: "user_sessions#destroy"
+  resources :password_resets, only: %i[new create edit update]
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
   # Can be used by load balancers and uptime monitors to verify that the app is live.
