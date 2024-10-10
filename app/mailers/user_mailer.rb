d0a99b729fe96a1a83d90bed6e8c46d62831dc23
Kotono Watanabe <kotakoto529@gmail.com>
2024-10-10 11:40:23 +0900

    Update: メール送信元アドレス変更

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,4 +1,6 @@
 class UserMailer < ApplicationMailer
+  default from: "artspotly@gmail.com"
+
   def reset_password_email(user)
     @user = User.find(user.id)
     @url  = edit_password_reset_url(@user.reset_password_token)
