feec15415fd6a4e51994adebc549c6c3e93ae072
Kotono Watanabe <kotakoto529@gmail.com>
2024-10-10 13:39:36 +0900

    Fix: 修正

--- app/controllers/password_resets_controller.rb
+++ app/controllers/password_resets_controller.rb
@@ -6,9 +6,9 @@ class PasswordResetsController < ApplicationController
   # request password reset.
   # you get here when the user entered their email in the reset password form and submitted it.
   def create
-    @user = User.find_by_email(params[:email])
+    @user = User.find_by(email: params[:email])
     # This line sends an email to the user with instructions on how to reset their password (a url with a random token)
-    @user.deliver_reset_password_instructions! if @user
+    @user&.deliver_reset_password_instructions!
     # Tell the user instructions have been sent whether or not email was found.
     # This is to not leak information to attackers about which emails exist in the system.
     redirect_to login_path, success: t(".success")
@@ -18,14 +18,13 @@ class PasswordResetsController < ApplicationController
   def edit
     @token = params[:id]
     @user = User.load_from_reset_password_token(@token)
-
     not_authenticated if @user.blank?
   end
 
   # This action fires when the user has sent the reset password form.
   def update
     @token = params[:id]
-    @user = User.load_from_reset_password_token(params[:id])
+    @user = User.load_from_reset_password_token(@token)
 
     return not_authenticated if @user.blank?
 
