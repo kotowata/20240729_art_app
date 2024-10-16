b6e60b639661400fdbcdd5581593e56773c0857d
Kotono Watanabe <kotakoto529@gmail.com>
2024-10-16 10:34:05 +0900

    test

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,3 +1,13 @@
 // Entry point for the build script in your package.json
 import "@hotwired/turbo-rails"
 import "./controllers"
+
+// ローディングアニメーションの表示
+document.addEventListener("DOMContentLoaded", () => {
+  const form = document.querySelector("form"); // 対象のフォームを選択
+  const loading = document.getElementById("loading");
+
+  form.addEventListener("submit", () => {
+    loading.style.display = "flex"; // ローディングを表示
+  });
+});
